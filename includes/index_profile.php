5eb7443a96dab0da91092eca8408d866148624ad
Francesc Xavier Caro Miro <xavicaromiro@gmail.com>
2017-11-29 03:59:22 +0100

    correction

--- includes/index_profile.php
+++ includes/index_profile.php
@@ -96,13 +96,83 @@ if ($num == 1) {
 		document.getElementById(''+4+'').classList.remove('fa-star-o');
 		document.getElementById(''+4+'').classList.add('fa-star');";
 		}
-		
 	} else {
 		echo '<a class="fa fa-star-o" style="color:#8E44AD;text-decoration:none;" aria-hidden="true" id="0" href="index.php?id='.$id.'&stars=1"></a>
 			<a class="fa fa-star-o" style="color:#8E44AD;text-decoration:none;" id="1" aria-hidden="true" href="index.php?id='.$id.'&stars=2"></a>
 			<a class="fa fa-star-o" style="color:#8E44AD;text-decoration:none;" id="2" aria-hidden="true" href="index.php?id='.$id.'&stars=3"></a>
 			<a class="fa fa-star-o" style="color:#8E44AD;text-decoration:none;" id="3" aria-hidden="true" href="index.php?id='.$id.'&stars=4"></a>
-			<a class="fa fa-star-o" style="color:#8E44AD;text-decoration:none;" id="4" aria-hidden="true" href="index.php?id='.$id.'&stars=5"></a>';
+			<a class="fa fa-star-o" style="color:#8E44AD;text-decoration:none;" id="4" aria-hidden="true" href="index.php?id='.$id.'&stars=5"></a><script>';
+		if ($avg <= 0.5) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star-half-o');";
+		} else if ($avg <= 1) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');";
+		} else if ($avg <= 1.5) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star-half-o');";
+		} else if ($avg <= 2) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');";
+		} else if ($avg <= 2.5) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');
+		document.getElementById(''+2+'').classList.remove('fa-star-o');
+		document.getElementById(''+2+'').classList.add('fa-star-half-o');";
+		} else if ($avg <= 3) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');
+		document.getElementById(''+2+'').classList.remove('fa-star-o');
+		document.getElementById(''+2+'').classList.add('fa-star');";
+		} else if ($avg <= 3.5) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');
+		document.getElementById(''+2+'').classList.remove('fa-star-o');
+		document.getElementById(''+2+'').classList.add('fa-star');
+		document.getElementById(''+3+'').classList.remove('fa-star-o');
+		document.getElementById(''+3+'').classList.add('fa-star-half-o');";
+		} else if ($avg <= 4) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');
+		document.getElementById(''+2+'').classList.remove('fa-star-o');
+		document.getElementById(''+2+'').classList.add('fa-star');
+		document.getElementById(''+3+'').classList.remove('fa-star-o');
+		document.getElementById(''+3+'').classList.add('fa-star');";
+		} else if ($avg <= 4.5) {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');
+		document.getElementById(''+2+'').classList.remove('fa-star-o');
+		document.getElementById(''+2+'').classList.add('fa-star');
+		document.getElementById(''+3+'').classList.remove('fa-star-o');
+		document.getElementById(''+3+'').classList.add('fa-star');
+		document.getElementById(''+4+'').classList.remove('fa-star-o');
+		document.getElementById(''+4+'').classList.add('fa-star-half-o');";
+		} else {
+			echo "document.getElementById(''+0+'').classList.remove('fa-star-o');
+		document.getElementById(''+0+'').classList.add('fa-star');
+		document.getElementById(''+1+'').classList.remove('fa-star-o');
+		document.getElementById(''+1+'').classList.add('fa-star');
+		document.getElementById(''+2+'').classList.remove('fa-star-o');
+		document.getElementById(''+2+'').classList.add('fa-star');
+		document.getElementById(''+3+'').classList.remove('fa-star-o');
+		document.getElementById(''+3+'').classList.add('fa-star');
+		document.getElementById(''+4+'').classList.remove('fa-star-o');
+		document.getElementById(''+4+'').classList.add('fa-star');";
+		}
 	}
 	echo '</script>
 	<small style="margin-left: 10px;">('.$row['average'].')</small></div>
