8b3221da616b570eb416ac1226f21e9520a923c9
Xavi <xavicaromiro@gmail.com>
2017-12-13 16:11:47 +0100

    same

--- includes/show_product.php
+++ includes/show_product.php
@@ -12,7 +12,16 @@ if (1 == $row['status']) {
 		if ($key == 0) echo '<div class="carousel-item active"><img class="d-block img-fluid" src="'.$img.'" alt="First slide"></div>';
 		else echo '<div class="carousel-item"><img class="d-block img-fluid" src="'.$img.'" alt="Second slide"></div>';
 	}
-	echo '</div><a class="carousel-control-prev" href="#carouselExampleIndicators'.$cont.'" role="button" data-slide="prev"><span class="carousel-control-prev-icon" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control-next" href="#carouselExampleIndicators'.$cont.'" role="button" data-slide="next"><span class="carousel-control-next-icon" aria-hidden="true"></span><span class="sr-only">Next</span></a></div>';
+	echo '</div>
+	<a class="carousel-control-prev" href="#carouselExampleIndicators'.$cont.'" role="button" data-slide="prev">
+		<span class="fa fa-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#carouselExampleIndicators'.$cont.'" role="button" data-slide="next">
+		<span class="fa fa-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+	</div>';
 	echo '</div><div class="col-sm-8 product-text"><div class="row product-title"><div class="col-sm-10">'.$row['name'].'</div>';
 	if (isset($myprofile)) echo '<div class="col-sm-2 text-right product-seller"><a href="edit_fig.php?fid='.$row['figure_id'].'"><span class="material-icons">mode_edit</span></a><a href="del_fig.php?fid='.$row['figure_id'].'"><span class="material-icons">delete_forever</span></a></div>';
 	echo '</div><hr><div class="row product-desc text-justify">'.$row['description'].'</div><hr><div class="row product-footer"><div class="col-sm-6 text-left product-price">'.$row['price'].'€</div><div class="col-sm-6 text-right product-seller"><a href="profile.php?id='.$row['username'].'">'.$row['username'].'</a></div></div></div></div>';
