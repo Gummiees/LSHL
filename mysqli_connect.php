2610243637a6450d2f3e16574bc58c76a1cecc70
Xavi <xavicaromiro@gmail.com>
2017-12-14 19:11:02 +0100

    new db

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -2,7 +2,7 @@
 DEFINE ('DB_USER', 'root');
 DEFINE ('DB_PASSWORD', '');
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'u787804074_lshl');
+DEFINE ('DB_NAME', 'u830493339_lshl');
 
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: '.mysqli_connect_error());
 
--- sql/u830493339_lshl.sql
+++ sql/u830493339_lshl.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-12-2017 a las 20:33:15
+-- Tiempo de generación: 14-12-2017 a las 17:16:14
 -- Versión del servidor: 10.1.28-MariaDB
 -- Versión de PHP: 7.1.10
 
@@ -78,6 +78,8 @@ CREATE TABLE `messages` (
   `title` varchar(50) NOT NULL,
   `description` varchar(1000) NOT NULL,
   `viewed` tinyint(1) NOT NULL DEFAULT '0',
+  `view_destiny` tinyint(1) NOT NULL DEFAULT '1',
+  `view_origin` tinyint(1) NOT NULL DEFAULT '1',
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -85,10 +87,14 @@ CREATE TABLE `messages` (
 -- Volcado de datos para la tabla `messages`
 --
 
-INSERT INTO `messages` (`message_id`, `origin_id`, `destiny_id`, `title`, `description`, `viewed`, `date`) VALUES
-(3, 4, 11, 'Hey it\'s me, CodeGayAss', 'How are you my dear friend? Today I talk to you to know if everything is allright. Hope it\'s a yes. See you later fam.', 0, '2017-12-13 19:59:39'),
-(4, 4, 12, 'Hey it\'s me, CodeGayAss', 'How are you my dear friend? Today I talk to you to know if everything is allright. Hope it\'s a yes. See you later fam.', 0, '2017-12-13 19:59:39'),
-(5, 10, 12, '1234567890123456789012345678901234567890123456789', '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890', 1, '2017-12-13 19:59:39');
+INSERT INTO `messages` (`message_id`, `origin_id`, `destiny_id`, `title`, `description`, `viewed`, `view_destiny`, `view_origin`, `date`) VALUES
+(4, 4, 12, 'Hey it\'s me, CodeGayAss', 'How are you my dear friend? Today I talk to you to know if everything is allright. Hope it\'s a yes. See you later fam.', 0, 1, 1, '2017-12-13 19:59:39'),
+(5, 10, 12, '1234567890123456789012345678901234567890123456789', '123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890', 1, 1, 1, '2017-12-13 19:59:39'),
+(6, 12, 3, 'Hello how are you my dear', 'asdadasdasdasdasdasassaasdasdas', 0, 1, 1, '2017-12-14 15:17:55'),
+(7, 12, 5, 'Message title', 'Message description', 1, 1, 0, '2017-12-14 15:53:36'),
+(8, 12, 13, 'MoguelMoguelMoguelMogu elMoguelMog', 'MoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguelMoguel', 1, 1, 1, '2017-12-14 15:55:45'),
+(9, 13, 12, 'hola', 'hola', 0, 1, 1, '2017-12-14 16:14:56'),
+(11, 12, 13, 'tonto', 'tonot', 0, 1, 1, '2017-12-14 16:43:19');
 
 -- --------------------------------------------------------
 
@@ -114,7 +120,8 @@ INSERT INTO `stars` (`star_id`, `buyer_id`, `seller_id`, `value`) VALUES
 (7, 13, 1, 4),
 (8, 3, 2, 5),
 (9, 3, 1, 5),
-(10, 3, 12, 3);
+(10, 3, 12, 3),
+(11, 12, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -125,7 +132,7 @@ INSERT INTO `stars` (`star_id`, `buyer_id`, `seller_id`, `value`) VALUES
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
-  `username` varchar(20) NOT NULL,
+  `username` varchar(10) NOT NULL,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(40) NOT NULL,
   `email` varchar(60) NOT NULL,
@@ -143,7 +150,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`user_id`, `username`, `first_name`, `last_name`, `email`, `telephone`, `pass`, `image`, `description`, `registration_date`) VALUES
 (1, 'jhon001', 'John', 'Lennon', 'john@beatles.com', '654375123', '2a50435b0f512f60988db719106a258fb7e338ff', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/7e/30/c8/7e30c857-99f6-ed48-c4c8-f1387d2b992e/source/1200x630bb.jpg', 'Hey there! I\'m using Love Second Hand Live!', '2017-11-24 18:35:44'),
 (2, 'paulXD', 'Paul', 'McCartney', 'paul@beatles.com', '748346784', '6ae16792c502a5b47da180ce8456e5ae7d65e262', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/7e/30/c8/7e30c857-99f6-ed48-c4c8-f1387d2b992e/source/1200x630bb.jpg', 'Hey there! I\'m using Love Second Hand Live!', '2017-11-24 18:35:44'),
-(3, 'saoisthebest', 'George', 'Harrison', 'george@beatles.com ', '123123123', '711383a59fda05336fd2ccf70c8059d1523eb41a', 'https://t3.ftcdn.net/jpg/01/18/01/98/500_F_118019822_6CKXP6rXmVhDOzbXZlLqEM2ya4HhYzSV.jpg', 'Plz', '2017-11-24 18:35:44'),
+(3, 'saois', 'George', 'Harrison', 'george@beatles.com ', '123123123', '711383a59fda05336fd2ccf70c8059d1523eb41a', 'https://t3.ftcdn.net/jpg/01/18/01/98/500_F_118019822_6CKXP6rXmVhDOzbXZlLqEM2ya4HhYzSV.jpg', 'Plz', '2017-11-24 18:35:44'),
 (4, 'CodeGayAss', 'Ringo', 'Starr', 'ringo@beatles.com', '768567890', '520f73691bcf89d508d923a2dbc8e6fa58efb522', 'https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/7e/30/c8/7e30c857-99f6-ed48-c4c8-f1387d2b992e/source/1200x630bb.jpg', 'Hey there! I\'m using Love Second Hand Live!', '2017-11-24 18:35:44'),
 (5, 'pablito', 'Pablo', 'Motos', 'pablo_motos@yahoo.es', '654738291', 'pablito1', 'https://donbalonrosa.defensacentral.com/images/2017/09/07/1504799606.jpg', 'Hey que tal guapas ;)', '2017-11-28 00:00:00'),
 (10, 'morgan', 'Morgan', 'Suisa', 'morgan@dasuisa.cat', '420420420', '9706377a84ddf99fa147d587c02c6a684c7a3465', 'https://vignette.wikia.nocookie.net/creation/images/2/22/ANGRY_MARGE_BECH.jpg', 'NO TE PASES DE LISTO CON MIGO.', '2017-11-28 18:25:40'),
@@ -200,13 +207,13 @@ ALTER TABLE `figures`
 -- AUTO_INCREMENT de la tabla `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `message_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `message_id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `stars`
 --
 ALTER TABLE `stars`
-  MODIFY `star_id` mediumint(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `star_id` mediumint(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
